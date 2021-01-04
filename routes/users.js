d5ad4b8b702e746af40b3158d22832d6db6d0b26
Nitzan Maizel <nitzanmaizel@gmail.com>
2021-01-04 13:21:06 +0200

    Check if user exist

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { check, validationResult } = require('express-validator');
+const { findOne } = require('../models/User');
 
 // @route    POST api/users
 // @desc     Signup user
@@ -14,12 +15,23 @@ router.post(
 		check('email', 'Please enter valid email').isEmail(),
 		check('password', 'Password must be greater than 6 character').isLength({ min: 6 }),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send(req.body);
+		const { name, email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ msg: 'User already exist' });
+			}
+			user = new User({ name, email, password });
+		} catch (err) {
+			console.error(err.massage);
+			res.status(500).json({ msg: 'Server error' });
+		}
 	}
 );
 
